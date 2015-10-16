4ad09e74d8063ae675f117c324303890f2555bbe
chismebind <chismebind@gmail.com>
2015-10-16 15:39:41 +0000

    simple functions

---
+++ app.js
@@ -0,0 +1,23 @@
+		  var myDataRef = new Firebase('https://incandescent-inferno-8327.firebaseio.com/');
+		  $('#messageInput').keypress(function (e) {
+		    if (e.keyCode == 13) {
+		      var name = $('#nameInput').val();
+		      var text = $('#messageInput').val();
+		      
+		     	myDataRef.push({name: name, text: text});
+
+		      $('#messageInput').val('');
+		    }
+		  });
+
+		  myDataRef.on('child_added', function(snapshot) {
+			  var message = snapshot.val();
+			  displayChatMessage(message.name, message.text);
+			});
+
+		  function displayChatMessage(name, text) {
+		    $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+		    $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;	
+			};
+
+app
